16c157bdd3c7ff34ebba6cb01d895d34fb957624
vinicius0alves <viniciusmartalves@gmail.com>
2024-01-11 08:07:41 -0300

    adicao do kafkafeedbackproducer e ajuste no kafkaadviceconsumer

---
+++ IDS's/ids/src/main/java/br/com/ids/producer/KafkaFeedbackProducer.java
@@ -0,0 +1,28 @@
+package br.com.ids.producer;
+
+import br.com.ids.dto.ConselorsDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaFeedbackProducer {
+
+    private final String topic;
+    private final Logger logg = LoggerFactory.getLogger(KafkaFeedbackProducer.class);
+    @Autowired
+    private final KafkaTemplate<String, ConselorsDTO> kafkaTemplate;
+
+    public KafkaFeedbackProducer(KafkaTemplate<String, ConselorsDTO> kafkaTemplate){
+        this.topic = "FEEDBACK_TOPIC";
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(ConselorsDTO data){
+        kafkaTemplate.send(topic, data);
+    } //adaptar para enviar apenas feedback informando se agregou ou nao o conselho
+}
