bd538ff8492dc0832bfebf297f4d8915b569cefe
Estêvão <estevaofcardoso@gmail.com>
2024-08-23 12:11:46 -0300

    Commit new Controller

---
+++ IDS's/ids2/src/main/java/br/com/ids/controller/StratProcessController.java
@@ -0,0 +1,30 @@
+package br.com.ids.controller;
+
+import br.com.ids.scheduling.JobScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/cnw")
+public class StratProcessController {
+
+    @Autowired
+    private JobScheduler jobScheduler;
+
+    @PostMapping("/start")
+    public ResponseEntity<String> startTrainingProcess() {
+        try {
+            jobScheduler.startProcess();
+            return ResponseEntity.ok("Successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body("Failed to start execution: " + e.getMessage());
+        }
+    }
+
+}
